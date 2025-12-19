515425bf70a4342a97faf2c16ec97b928fe7f548
Ebin234 <ebinyesudas234@gmail.com>
2025-12-19 14:51:31 +0000

    Nft flip function added and DeployMoodNft contract created

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -5,16 +5,18 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__CantFlipMoodIfNotOwner();
+
     string private s_sadSvgImageUri;
     string private s_happySvgImageUri;
     uint256 private s_tokenCounter;
+
+    mapping(uint256 => Mood) s_tokenIdToMood;
 
     enum Mood {
         HAPPY,
         SAD
     }
-
-    mapping(uint256 => Mood) s_tokenIdToMood;
 
     constructor(
         string memory sadSvgUri,
@@ -63,5 +65,17 @@ contract MoodNft is ERC721 {
 
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
+    }
+
+    function flipMood(uint256 tokenId) public {
+        if(getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender){
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+
+        if(s_tokenIdToMood[tokenId] == Mood.HAPPY){
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        }else{
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 }
